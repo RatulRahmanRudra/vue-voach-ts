4a5e5e7290a5d3544dfdaca4c4e26410f63e6f17
Ratul Rahman Rudra <44123571+RatulRahmanRudra@users.noreply.github.com>
2021-07-15 01:37:15 +0600

    init-commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <AppHeader/>
+  <router-view />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import AppHeader from '@/components/layout/AppHeader.vue'
+
+export default defineComponent({
+  components: { AppHeader },
+  data(){
+    return {}
+  },
+  
+})
+</script>
---
+++ src/components/ui/Card.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="border w-7/12 m-auto rounded-xl text-center p-4 my-5">
+    <slot></slot>
+  </div>
+</template>
